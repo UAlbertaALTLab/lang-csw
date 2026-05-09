dd438962435fd6a13a5838e7fe7db9f8e1f01aa7
Elora20 <elora02@mit.edu>
2026-05-09 10:40:46 -0600

    added nouns to the lexc

--- fst/morphology/scripts/compile-foma-fsts.sh
+++ fst/morphology/scripts/compile-foma-fsts.sh
@@ -13,6 +13,10 @@ cat \
 ./affixes/verb_prefixes.lexc \
 ./stems/verb_stems.lexc \
 ./affixes/verb_suffixes.lexc \
+./affixes/noun_prefixes.lexc \
+./affixes/prenouns.lexc \
+./stems/noun_stems.lexc \
+./affixes/noun_suffixes.lexc \
 > lexicon.lexc
 
 echo 'Compiling FOMA FSTs.' ;
